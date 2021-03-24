8eb0e84f383230cc5e733b029216237d564c8ee0
Joseph Dubon <josephdubon@pm.me>
2021-03-24 13:24:54 -0700

    Added serializers for: NSManufacturerSerializer NSShoeTypeSerializer NSShoeColorSerializer NSShoeSerializer

--- nutshell_config/nutshell_app/serializers.py
+++ nutshell_config/nutshell_app/serializers.py
@@ -1,14 +1,50 @@
 from django.contrib.auth.models import User, Group
+from nutshell_config.nutshell_app import models
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url',
+                  'username',
+                  'email',
+                  'groups']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class NSManufacturerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.NSManufacturer
+        fields = ['name', 'website']
+
+
+class NSShoeTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.NSShoeType
+        fields = ['style', ]
+
+
+class NSShoeColorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.NSShoeColor
+        fields = ['color_name', ]
+
+
+class NSShoeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.NSShoe
+        fields = [
+            'size',
+            'brand_name',
+            'manufacturer',
+            'color',
+            'material',
+            'shoe_type',
+            'fasten_type',
+        ]
