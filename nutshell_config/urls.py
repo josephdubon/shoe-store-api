af0c794976320c8ec0cfc474310cc1b511d38568
Joseph Dubon <josephdubon@pm.me>
2021-03-24 15:46:05 -0700

    Added spacing between import types. Added import and path for admin + admin panel.

--- nutshell_config/urls.py
+++ nutshell_config/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
+
 from rest_framework import routers
+
 from nutshell_config.nutshell_app import views
 
 router = routers.DefaultRouter()
@@ -28,6 +31,7 @@ router.register(r'shoes', views.NSShoeViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
