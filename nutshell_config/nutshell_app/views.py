ba5edbe91663bb3052ba606e6e40b781c3c98ab0
Joseph Dubon <josephdubon@pm.me>
2021-03-24 13:26:21 -0700

    Added viewsets for: NSManufacturer NSShoeType NSShoeColor NSShoe

--- nutshell_config/nutshell_app/views.py
+++ nutshell_config/nutshell_app/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from nutshell_config.nutshell_app.serializers import UserSerializer, GroupSerializer
+from nutshell_config.nutshell_app import serializers
+from nutshell_config.nutshell_app import models
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -9,7 +10,7 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
@@ -18,5 +19,37 @@ class GroupViewSet(viewsets.ModelViewSet):
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    serializer_class = serializers.GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class NSManufacturerViewSet(viewsets.ModelViewSet):
+    """
+    API end point that allows Manufacturer to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = serializers.NSManufacturerSerializer
+
+
+class NSShoeTypeViewSet(viewsets.ModelViewSet):
+    """
+    API end point that allows Manufacturer to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = serializers.NSShoeTypeSerializer
+
+
+class NSShoeColorViewSet(viewsets.ModelViewSet):
+    """
+    API end point that allows Manufacturer to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = serializers.NSShoeColorSerializer
+
+
+class NSShoeViewSet(viewsets.ModelViewSet):
+    """
+    API end point that allows Manufacturer to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = serializers.NSShoeSerializer
