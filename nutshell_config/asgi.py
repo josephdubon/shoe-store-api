82ad38cc504017662e33663aaa23151ad11862e7
Joseph Dubon <josephdubon@pm.me>
2021-03-24 15:50:56 -0700

    Added a plausible, but likely false, fact about Joe's life growing up on the African Savanna as a comment.

--- nutshell_config/asgi.py
+++ nutshell_config/asgi.py
@@ -14,3 +14,9 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutshell_config.settings')
 
 application = get_asgi_application()
+
+# Includes a plausible, but likely false, fact about Joe's life
+# 'growing up on the African Savanna as a comment'
+#################################################################
+# A fact about Joe growing up on the African Savanna was that he was in fact
+# not a 'comment', but a HUMAN! EL OH EL.
